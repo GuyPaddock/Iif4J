3eaf113a3180d1d75933698bcbb489c20291ea57
Guy Paddock <guy.paddock@redbottledesign.com>
2016-10-03 09:59:00 -0400

    Combines `TxnType.Type` with `TxnType`, since enumerations can implement interfaces.

--- src/com/redbottledesign/accounting/quickbooks/models/TxnType.java
+++ src/com/redbottledesign/accounting/quickbooks/models/TxnType.java
@@ -20,48 +20,116 @@ import com.redbottledesign.accounting.quickbooks.iif.IifExportable;
 import com.redbottledesign.accounting.quickbooks.util.IifUtils;
 
 /**
- * The type of transaction.
+ * An enumeration of the types of transaction types that QuickBooks supports.
  *
  * @author Guy Paddock (guy@redbottledesign.com)
  */
-public class TxnType
+public enum TxnType
 implements IifExportable {
     /**
-     * The type of the transaction, wrapped in this object.
+     * Transactions that create a beginning balance in a balance sheet
+     * account.
      */
-    private Type value;
+    BEGIN_BALANCE_CHECK("BEGINBALCHECK"),
 
     /**
-     * Constructor for {@code TxnType}.
-     *
-     * @param   value
-     *          The transaction type being wrapped.
+     * Bills from vendors.
      */
-    public TxnType(final Type value) {
-        this.setValue(value);
+    BILL("BILL"),
+
+    /**
+     * Refunds from a vendor.
+     */
+    BILL_REFUND("BILL REFUND"),
+
+    /**
+     * Cash refunds you give to customers.
+     */
+    CASH_REFUND("CASH REFUND"),
+
+    /**
+     * Sales receipts.
+     */
+    CASH_SALE("CASH SALE"),
+
+    /**
+     * Refunds you receive on credit card charges.
+     */
+    CREDIT_CARD_REFUND("CCARD REFUND"),
+
+    /**
+     * Checks.
+     */
+    CHECK("CHECK"),
+
+    /**
+     * Charges you make on a credit card.
+     */
+    CREDIT_CARD("CREDIT CARD"),
+
+    /**
+     * Credit you give to customers for merchandise they return.
+     */
+    CREDIT_MEMO("CREDIT MEMO"),
+
+    /**
+     * Bank or money market deposits.
+     */
+    DEPOSIT("DEPOSIT"),
+
+    /**
+     * Estimates or bids.
+     */
+    ESTIMATE("ESTIMATES"),
+
+    /**
+     * General journal transactions.
+     */
+    GENERAL_JOURNAL("GENERAL JOURNAL"),
+
+    /**
+     * Invoices.
+     */
+    INVOICE("INVOICE"),
+
+    /**
+     * Customer payments.
+     */
+    PAYMENT("PAYMENT"),
+
+    /**
+     * Purchase orders.
+     */
+    PURCHASE_ORDER("PURCHORD"),
+
+    /**
+     * Transfers of funds from one balance sheet account to another.
+     */
+    TRANSFER("TRANSFER");
+
+    /**
+     * The internal identifier for this option in QuickBooks.
+     */
+    private final String qbIdentifier;
+
+    /**
+     * Private, enum constrcutor for @{link Type}
+     *
+     * @param   qbIdentifier
+     *          The identifier for this type in QuickBooks.
+     */
+    TxnType(String qbIdentifier) {
+        this.qbIdentifier = qbIdentifier;
     }
 
     /**
-     * Gets the type of transaction.
+     * Gets the unique identifier for this type of transaction.
      *
-     * @return  The transaction type.
+     * @return  String
+     *          The QuickBooks code for this transaction type.
      */
-    public Type getValue() {
-        return this.value;
-    }
-
-    /**
-     * Sets the type of transaction.
-     *
-     * @param   type
-     *          The new transaction type.
-     */
-    protected void setValue(final Type type) {
-        if (type == null) {
-            throw new IllegalArgumentException("value cannot be null");
-        }
-
-        this.value = type;
+    public String getQbIdentifier() {
+        return this.qbIdentifier;
     }
 
     /**
@@ -73,118 +141,6 @@ implements IifExportable {
      */
     @Override
     public String toIifString() {
-        return IifUtils.escapeColumn(this.getValue().getQbIdentifier());
-    }
-
-    /**
-     * An enumeration of the types of transaction types that QuickBooks
-     * supports.
-     */
-    public enum Type {
-        /**
-         * Transactions that create a beginning balance in a balance sheet
-         * account.
-         */
-        BEGIN_BALANCE_CHECK("BEGINBALCHECK"),
-
-        /**
-         * Bills from vendors.
-         */
-        BILL("BILL"),
-
-        /**
-         * Refunds from a vendor.
-         */
-        BILL_REFUND("BILL REFUND"),
-
-        /**
-         * Cash refunds you give to customers.
-         */
-        CASH_REFUND("CASH REFUND"),
-
-        /**
-         * Sales receipts.
-         */
-        CASH_SALE("CASH SALE"),
-
-        /**
-         * Refunds you receive on credit card charges.
-         */
-        CREDIT_CARD_REFUND("CCARD REFUND"),
-
-        /**
-         * Checks.
-         */
-        CHECK("CHECK"),
-
-        /**
-         * Charges you make on a credit card.
-         */
-        CREDIT_CARD("CREDIT CARD"),
-
-        /**
-         * Credit you give to customers for merchandise they return.
-         */
-        CREDIT_MEMO("CREDIT MEMO"),
-
-        /**
-         * Bank or money market deposits.
-         */
-        DEPOSIT("DEPOSIT"),
-
-        /**
-         * Estimates or bids.
-         */
-        ESTIMATE("ESTIMATES"),
-
-        /**
-         * General journal transactions.
-         */
-        GENERAL_JOURNAL("GENERAL JOURNAL"),
-
-        /**
-         * Invoices.
-         */
-        INVOICE("INVOICE"),
-
-        /**
-         * Customer payments.
-         */
-        PAYMENT("PAYMENT"),
-
-        /**
-         * Purchase orders.
-         */
-        PURCHASE_ORDER("PURCHORD"),
-
-        /**
-         * Transfers of funds from one balance sheet account to another.
-         */
-        TRANSFER("TRANSFER");
-
-        /**
-         * The internal identifier for this option in QuickBooks.
-         */
-        private final String qbIdentifier;
-
-        /**
-         * Private, enum constrcutor for @{link Type}
-         *
-         * @param   qbIdentifier
-         *          The identifier for this type in QuickBooks.
-         */
-        Type(String qbIdentifier) {
-            this.qbIdentifier = qbIdentifier;
-        }
-
-        /**
-         * Gets the unique identifier for this type of transaction.
-         *
-         * @return  String
-         *          The QuickBooks code for this transaction type.
-         */
-        public String getQbIdentifier() {
-            return this.qbIdentifier;
-        }
+        return IifUtils.escapeColumn(this.getQbIdentifier());
     }
 }
